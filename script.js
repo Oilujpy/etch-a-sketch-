d52b78f0b53231751ca13117c06e5338c913467f
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-04-28 17:30:24 -0400

    feat: implement hover logic

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const containerDiv = document.querySelector(".container");
 
+const createSquare = function() {
+    const square = document.createElement("div");
+    return square;
+}
+
+const handleHover = function(element) {
+    element.addEventListener("mouseover", ()=> {
+        element.setAttribute("style", "background: #eab676");
+    })
+} 
+
 const fillGrid = function (squareNumber) {
-    const squareDiv = document.createElement("div");
+    const squareDiv = createSquare();
     squareDiv.setAttribute("class", "square");
     squareDiv.textContent = squareNumber;
     containerDiv.appendChild(squareDiv);    
+    handleHover(squareDiv);
 } 
 
 for (let index = 1; index < 256 + 1; index++) {
