12281b3870e1ce6ed8702460f0a1b6ee5cfa467e
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-04-30 12:15:56 -0400

    feat: add button to capture user input
    ** Created a function to calculate the size of the square based on the
    user input.
    ** Used the user input to calcualte the numbers of square needed.
    ** Removed css properties that were impacting the square size
    to streamline the calculation.

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 const containerDiv = document.querySelector(".container");
+const button = document.querySelector("#changeSizeBtn");
 
-const createSquare = function() {
+const calculateSquareSize = function (userInput) {
+    const MAX_WIDTH = 1000;
+    let size = (MAX_WIDTH / userInput);
+    return size;
+}
+
+const createSquare = function () {
     const square = document.createElement("div");
     return square;
 }
 
-const handleHover = function(element) {
-    element.addEventListener("mouseover", ()=> {
-        element.setAttribute("style", "background: #eab676");
+const handleHover = function (element) {
+    element.addEventListener("mouseover", () => {
+        element.style.backgroundColor = "#eab676";
     })
-} 
+}
 
-const fillGrid = function (squareNumber) {
+
+button.addEventListener("click", () => {
+    let input = Number(prompt("Enter a number equal or smaller than 100:"));
+    if (input > 100) {
+        alert("The number must be smaller than 100. Try again!");
+        input = Number(prompt("Enter a number smaller than 100:"));        
+    }
+})
+
+const test = 100;
+console.log(calculateSquareSize(test));
+
+
+const fillGrid = function () {
     const squareDiv = createSquare();
     squareDiv.setAttribute("class", "square");
-    squareDiv.textContent = squareNumber;
-    containerDiv.appendChild(squareDiv);    
+    squareDiv.setAttribute("style", `width: ${calculateSquareSize(test)}px; height: ${calculateSquareSize(test)}px`);
+    containerDiv.appendChild(squareDiv);
     handleHover(squareDiv);
-} 
+}
 
-for (let index = 1; index < 256 + 1; index++) {
-    fillGrid(index);
+for (let index = 1; index < Math.pow(test ,2) + 1; index++) {
+    fillGrid();
 }
